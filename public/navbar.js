0e9aaef4f1f5093cc95b383efde167cb9b4d8567
meagan-davies <128192488+meagan-davies@users.noreply.github.com>
2024-07-17 10:30:06 -0600

    navbar + footer update

---
+++ public/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggle = document.getElementById('navbarToggle');
+    const navbarMenu = document.getElementById('navbarMenu');
+
+    navbarToggle.addEventListener('click', function () {
+        navbarMenu.classList.toggle('active');
+    });
+});
